55a06bd37b77bfaec9c3b229b1868b19c4f347c2
SX-7 <sn1.test.preria.2002@gmail.com>
2023-09-01 11:27:27 +0200

    Some more polish

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "hellow World?!??!"
+
+
+if __name__ == "main":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
