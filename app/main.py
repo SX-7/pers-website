fe4c11cbf1b8901d8b83d96885a41e46f515f04f
SX-7 <sn1.test.preria.2002@gmail.com>
2023-09-02 01:36:03 +0200

    Some code formatting. Need to install tailwind-prettier

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
-if bool(os.environ.get("DEVELOPMENT_ENV",False)):
-    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #Dev-only
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
+if bool(os.environ.get("DEVELOPMENT_ENV", False)):
+    app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0  # Dev-only
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 
 @app.route("/")
 def hello():
