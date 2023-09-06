fb499319e2bf69b93b89b1f6b9ffa162deb8a137
SX-7 <sn1.test.preria.2002@gmail.com>
2023-09-06 10:44:12 +0200

    dev setup now overwrites

--- dev_setup.sh
+++ dev_setup.sh
@@ -6,15 +6,15 @@ source .venv/bin/activate
 pip install -r app/requirements.txt --require-virtualenv --disable-pip-version-check
 npm install
 echo ''
-echo 'Setup config files and env vars'
+echo 'Seting up config files and env vars...'
 echo ''
-echo 'plugins' >> .prettierrc
+echo 'plugins' > .prettierrc
 echo '  - "prettier-plugin-tailwindcss"' >> .prettierrc
 cp .gitignore .prettierignore
 export DEVELOPMENT_ENV="True"
 echo ""
 echo "Remember to also run:"
 echo "npx tailwindcss -i ./app/static/css/style.css -o ./app/static/css/tailwind.css --watch"
-echo "for hot reload of css!"
+echo "for hot reload of css, and use prettier for code legibility"
 echo ""
 flask --app app/main.py run --reload
