5da69b204a6558f4e7ff7e696d25cfd9d183c996
SX-7 <sn1.test.preria.2002@gmail.com>
2023-09-06 10:36:03 +0200

    Added npm, expanded setup script

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # A very nice website 🙂
-Code for my own portfolio/personal website. 
+
+Code for my own portfolio/personal website.
 
 Currently hosted at [sx7.dev](https://sx7.dev) with GCL App Engine, planning to slowly but surely update it over time.
 
 "Tech" "stack" currently consists of:
+
 - Python ([Flask](https://flask.palletsprojects.com/en/2.3.x/))
 - [Alpine](https://alpinejs.dev/)
 - [Tailwind](https://tailwindcss.com)
--- dev_setup.sh
+++ dev_setup.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+echo 'Activate venvs, install dev deps'
+echo ''
 python -m venv .venv
 source .venv/bin/activate
 pip install -r app/requirements.txt --require-virtualenv --disable-pip-version-check
+npm install
+echo ''
+echo 'Setup config files and env vars'
+echo ''
+echo 'plugins' >> .prettierrc
+echo '  - "prettier-plugin-tailwindcss"' >> .prettierrc
+cp .gitignore .prettierignore
 export DEVELOPMENT_ENV="True"
 echo ""
 echo "Remember to also run:"
-echo "tailwindcss -i ./app/static/css/style.css -o ./app/static/css/tailwind.css --watch"
+echo "npx tailwindcss -i ./app/static/css/style.css -o ./app/static/css/tailwind.css --watch"
 echo "for hot reload of css!"
 echo ""
 flask --app app/main.py run --reload
