4a3938de806e191b021dd5dba526de5532d76a5e
SX-7 <sn1.test.preria.2002@gmail.com>
2023-09-07 15:44:46 +0200

    Added hover fix for mobiles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/templates/*.{html,js}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {
       colors: {
