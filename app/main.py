05102f6f5a3cf5800701bd8bb137f5d80e00bd42
SX-7 <sn1.test.preria.2002@gmail.com>
2023-09-22 14:44:29 +0200

    Added more pages, templates, link buttons

--- app/main.py
+++ app/main.py
@@ -6,10 +6,25 @@ if bool(os.environ.get("DEVELOPMENT_ENV", False)):
     app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0  # Dev-only
     app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def home():
+    current_page = "Home"
+    return render_template("index.html", page=current_page)
+
+@app.route("/links")
+def links():
+    current_page = "Links"
+    return render_template("links.html", page=current_page)
+
+@app.route("/projects")
+def projects():
+    current_page = "Projects"
+    return render_template("projects.html", page=current_page)
+
+@app.route("/socials")
+def socials():
+    current_page = "Socials"
+    return render_template("socials.html", page=current_page)
 
 
 if __name__ == "__main__":
