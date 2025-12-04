3ba738cbbafa127df503e9369429807aecb3f019
SX_7 <92227810+SX-7@users.noreply.github.com>
2025-12-04 10:37:41 +0000

    yeetus updatus

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,16 @@
 const yaml = require("js-yaml");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Pass through static images/fonts so they get copied to _site
-  eleventyConfig.addPassthroughCopy({"app/static" : "static"});
-  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+  eleventyConfig.addPassthroughCopy({ "app/static": "static" });
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
 
   return {
     dir: {
       input: "app/pages",
-      output: "_site",    // The folder Firebase will deploy
+      output: "_site", // The folder Firebase will deploy
       data: "../_data",
-      includes: "app/elements"
-    }
+      includes: "app/elements",
+    },
   };
 };
