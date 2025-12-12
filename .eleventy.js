9f87be8139d207d1e3fe22ace1966e0be9f642e1
SX_7 <92227810+SX-7@users.noreply.github.com>
2025-12-12 12:48:18 +0100

    Fixing lighthouse-ci score (#31)
    * idk why this is here
    
    * Expanded urls to all
    
    * lets see this rn
    
    * prettier
    
    * More yeeting
    
    * lets see if this is faster
    
    * let's see if this one doesn't break
    
    * oh yeah this
    
    * go my scarab
    
    * lets try this one now
    
    * Added preconnect
    
    * lets try this now
    
    * Maybe fixed the issues
    
    * Fix images and caching?
    
    * Hopefully now it'll work??
    
    * duh
    
    * Make it smaller
    
    * Yeet more!
    
    * right, prettier
    
    * Let's worry less about prettier
    
    * maybe this will work now idk
    
    * maybe this lmao
    
    * whatever fix it later
    
    * Should be ok now
    
    * testing...
    
    * oh yeah lint this too

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,77 @@
 const yaml = require("js-yaml");
+const postcss = require("postcss");
+const cssnano = require("cssnano");
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+const htmlmin = require("html-minifier-terser");
 
-module.exports = function (eleventyConfig) {
-  // Pass through static images/fonts so they get copied to _site
-  eleventyConfig.addPassthroughCopy({ "app/static": "static" });
+module.exports = (eleventyConfig) => {
+  // -----------------------------------------------------------------
+  // FILTERS
+  // -----------------------------------------------------------------
+  eleventyConfig.addNunjucksAsyncFilter("cssmin", function (code, callback) {
+    postcss([cssnano])
+      .process(code, { from: undefined })
+      .then((result) => callback(null, result.css))
+      .catch((error) => callback(error, null));
+  });
+
+  // -----------------------------------------------------------------
+  // EXTENSIONS & PLUGINS
+  // -----------------------------------------------------------------
+  // Support YAML data files
   eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    // outputs
+    formats: ["webp"],
 
+    filenameFormat: function (id, src, width, format) {
+      const path = require("path");
+      const name = path.basename(src, path.extname(src));
+      return `${name}-${width}w.${format}`;
+    },
+    selector: "img[src]:not([src$='.ico'])",
+  });
+
+  // -----------------------------------------------------------------
+  // TRANSFORMS
+  // -----------------------------------------------------------------
+  eleventyConfig.addTransform("htmlmin", async function (content) {
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+      // html-minifier-terser is ASYNC
+      let minified = await htmlmin.minify(content, {
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true, // defer="" -> defer
+        removeScriptTypeAttributes: true, // Removes type="text/javascript"
+        removeStyleLinkTypeAttributes: true, // Removes type="text/css"
+        removeRedundantAttributes: true,
+        decodeEntities: true,
+        minifyJS: true,
+        sortAttributes: true,
+        sortClassName: true,
+      });
+
+      return minified;
+    }
+
+    return content;
+  });
+
+  // -----------------------------------------------------------------
+  // PASSTHROUGH COPIES
+  // -----------------------------------------------------------------
+  // Copy 'app/static' to '_site/static'
+  eleventyConfig.addPassthroughCopy({ "app/static": "static" });
+
+  // -----------------------------------------------------------------
+  // CONFIGURATION OPTIONS
+  // -----------------------------------------------------------------
   return {
     dir: {
       input: "app/pages",
-      output: "_site", // The folder Firebase will deploy
-      data: "../_data",
-      includes: "app/elements",
+      includes: "app/elements", // Note: This path is relative to the root
+      data: "../_data", // Relative to 'input' (app/pages)
+      output: "_site",
     },
   };
 };
