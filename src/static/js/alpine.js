3ed3b7919ef5c884d67f66753db273243f54fc4e
SX_7 <92227810+SX-7@users.noreply.github.com>
2026-01-15 12:33:13 +0000

    Oh yeah it should update now

--- src/static/js/alpine.js
+++ src/static/js/alpine.js
@@ -1,9 +1,17 @@
 import Alpine from "alpinejs";
+import { firebaseAuth } from "app.js";
 
 window.Alpine = Alpine;
 
 Alpine.store("auth", {
   user: false,
+
+  init() {
+    // Firebase Auth Listener
+    firebaseAuth.onAuthStateChanged((user) => {
+      this.user = user;
+    });
+  },
 });
 
 Alpine.start();
