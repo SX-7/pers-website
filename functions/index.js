4ee04b238e4943533d6b6c60c44f5c8494c68f93
SX_7 <git@sx7.dev>
2026-02-26 16:19:21 +0000

    Nuke some stuff and update the function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 const { onRequest } = require("firebase-functions/v2/https");
-const { defineSecret } = require("firebase-functions/params");
+const { defineSecret, defineString } = require("firebase-functions/params");
 const express = require("express");
 const { AuthorizationCode } = require("simple-oauth2");
 const randomstring = require("randomstring");
@@ -9,35 +9,42 @@ const cookieParser = require("cookie-parser");
 const OAUTH_CLIENT_ID = defineSecret("OAUTH_CLIENT_ID");
 const OAUTH_CLIENT_SECRET = defineSecret("OAUTH_CLIENT_SECRET");
 
-const OAUTH_PROVIDER = process.env.OAUTH_PROVIDER || "github";
-const OAUTH_GIT_HOSTNAME =
-  process.env.OAUTH_GIT_HOSTNAME || "https://github.com";
-const OAUTH_TOKEN_PATH =
-  process.env.OAUTH_TOKEN_PATH || "/login/oauth/access_token";
-const OAUTH_AUTHORIZE_PATH =
-  process.env.OAUTH_AUTHORIZE_PATH || "/login/oauth/authorize";
-const OAUTH_REDIRECT_URL = process.env.OAUTH_REDIRECT_URL;
-const OAUTH_SCOPES = process.env.OAUTH_SCOPES || "repo,user";
+const OAUTH_PROVIDER = defineString("OAUTH_PROVIDER", { default: "github" });
+const OAUTH_GIT_HOSTNAME = defineString("OAUTH_GIT_HOSTNAME", {
+  default: "https://github.com",
+});
+const OAUTH_TOKEN_PATH = defineString("OAUTH_TOKEN_PATH", {
+  default: "/login/oauth/access_token",
+});
+const OAUTH_AUTHORIZE_PATH = defineString("OAUTH_AUTHORIZE_PATH", {
+  default: "/login/oauth/authorize",
+});
+const OAUTH_REDIRECT_URL = defineString("OAUTH_REDIRECT_URL"); // Requires a value at deploy time
+const OAUTH_SCOPES = defineString("OAUTH_SCOPES", { default: "repo,user" });
+
+const CMS_ORIGIN = defineString("CMS_ORIGIN", { default: "https://sx7.dev" });
 
 const oauthApp = express();
 
 function getScript(mess, content) {
+  const origin = CMS_ORIGIN.value();
+
   return `<!doctype html><html><body><script>
   (function() {
     function receiveMessage(e) {
-      if (e.origin !== '${CMS_ORIGIN}') return; 
+      if (e.origin !== '${origin}') return; 
       
       console.log("receiveMessage %o", e)
       window.opener.postMessage(
         'authorization:github:${mess}:${JSON.stringify(content)}',
-        '${CMS_ORIGIN}'
+        '${origin}'
       )
       window.removeEventListener("message", receiveMessage, false);
     }
     window.addEventListener("message", receiveMessage, false)
     console.log("Sending message: %o", "github")
     
-    window.opener.postMessage("authorizing:github", '${CMS_ORIGIN}') 
+    window.opener.postMessage("authorizing:github", '${origin}') 
     })()
   </script></body></html>`;
 }
@@ -49,30 +56,30 @@ function getOAuthClient() {
       secret: OAUTH_CLIENT_SECRET.value(),
     },
     auth: {
-      tokenHost: OAUTH_GIT_HOSTNAME,
-      tokenPath: OAUTH_TOKEN_PATH,
-      authorizePath: OAUTH_AUTHORIZE_PATH,
+      tokenHost: OAUTH_GIT_HOSTNAME.value(),
+      tokenPath: OAUTH_TOKEN_PATH.value(),
+      authorizePath: OAUTH_AUTHORIZE_PATH.value(),
     },
   });
 }
 
 oauthApp.use(cookieParser());
-oauthApp.use(cors({ origin: "https://sx7.dev" }));
+oauthApp.use(cors({ origin: true }));
 
 oauthApp.get("/auth", (req, res) => {
   const oauth2Client = getOAuthClient();
   const state = randomstring.generate(32);
 
   res.cookie("oauth_state", state, {
-    maxAge: 300000, // 5 minutes (plenty of time to log in)
+    maxAge: 300000, // 5 minutes
     httpOnly: true,
     secure: true,
     sameSite: "none",
   });
 
   const authorizationUri = oauth2Client.authorizeURL({
-    redirect_uri: OAUTH_REDIRECT_URL,
-    scope: OAUTH_SCOPES,
+    redirect_uri: OAUTH_REDIRECT_URL.value(),
+    scope: OAUTH_SCOPES.value(),
     state: state,
   });
 
@@ -83,9 +90,10 @@ oauthApp.get("/callback", async (req, res) => {
   const returnedState = req.query.state;
   const savedState = req.cookies.oauth_state;
   const oauth2Client = getOAuthClient();
+
   const options = {
     code: req.query.code,
-    redirect_uri: OAUTH_REDIRECT_URL,
+    redirect_uri: OAUTH_REDIRECT_URL.value(),
   };
 
   res.clearCookie("oauth_state", { secure: true, sameSite: "none" });
@@ -97,7 +105,7 @@ oauthApp.get("/callback", async (req, res) => {
       .send(getScript("error", { message: "Invalid state parameter" }));
   }
 
-  if (OAUTH_PROVIDER === "gitlab") {
+  if (OAUTH_PROVIDER.value() === "gitlab") {
     options.client_id = OAUTH_CLIENT_ID.value();
     options.client_secret = OAUTH_CLIENT_SECRET.value();
     options.grant_type = "authorization_code";
@@ -108,7 +116,7 @@ oauthApp.get("/callback", async (req, res) => {
     return res.send(
       getScript("success", {
         token: accessToken.token.access_token,
-        provider: OAUTH_PROVIDER,
+        provider: OAUTH_PROVIDER.value(),
       }),
     );
   } catch (error) {
@@ -120,8 +128,8 @@ oauthApp.get("/callback", async (req, res) => {
 exports.oauth = onRequest(
   {
     secrets: [OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET],
-    maxInstances: 3, // <--- Prevents billing spikes
-    timeoutSeconds: 15, // <--- Kills stalled requests
+    maxInstances: 3,
+    timeoutSeconds: 15,
   },
   oauthApp,
 );
